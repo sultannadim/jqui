41ad9ebc56930120a62be0635b762065cbf0e22e
sultan nadim <sultan5218@gmail.com>
2021-12-16 19:14:59 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,148 @@
+$(document).ready(function(){
+
+// drag start
+$( "#draggable" ).draggable();
+// drag end
+
+// auto complete start
+var availableTags = [
+	  "html",
+	  "css",
+      "javascript",
+      "jquery",
+      "Asp",
+      "BASIC",
+      "C",
+      "C++",
+      "Clojure",
+    ];
+    $( "#tags" ).autocomplete({
+      source: availableTags
+    });
+// auto complete end
+
+// accordion start
+$( "#accordion" ).accordion({
+      collapsible: true
+    });
+// accordion end
+
+// select part start
+ $( "#selectable" ).selectable();
+// select part end
+
+// sort part start
+$( "#sortable" ).sortable();
+// sort part end
+
+// dialog start
+$( "#dialog" ).dialog({
+      autoOpen: false,
+      show: {
+        effect: "blind",
+        duration: 1000
+      },
+      hide: {
+        effect: "explode",
+        duration: 1000
+      }
+    });
+ 
+    $( "#opener" ).on( "click", function() {
+      $( "#dialog" ).dialog( "open" );
+    });
+// dialog end
+
+//slider start
+$( "#slider-range-max" ).slider({
+      range: "max",
+      min: 1,
+      max: 10,
+      value: 2,
+      slide: function( event, ui ) {
+        $( "#amount" ).val( ui.value );
+      }
+    });
+    $( "#amount" ).val( $( "#slider-range-max" ).slider( "value" ) );
+//slider end
+
+// date start
+ $( "#datepicker" ).datepicker();
+// date end
+
+// pogress start
+var progressTimer,
+      progressbar = $( "#progressbar" ),
+      progressLabel = $( ".progress-label" ),
+      dialogButtons = [{
+        text: "Cancel Download",
+        click: closeDownload
+      }],
+      dialog = $( "#dialog" ).dialog({
+        autoOpen: false,
+        closeOnEscape: false,
+        resizable: false,
+        buttons: dialogButtons,
+        open: function() {
+          progressTimer = setTimeout( progress, 2000 );
+        },
+        beforeClose: function() {
+          downloadButton.button( "option", {
+            disabled: false,
+            label: "Start Download"
+          });
+        }
+      }),
+      downloadButton = $( "#downloadButton" )
+        .button()
+        .on( "click", function() {
+          $( this ).button( "option", {
+            disabled: true,
+            label: "Downloading..."
+          });
+          dialog.dialog( "open" );
+        });
+ 
+    progressbar.progressbar({
+      value: false,
+      change: function() {
+        progressLabel.text( "Current Progress: " + progressbar.progressbar( "value" ) + "%" );
+      },
+      complete: function() {
+        progressLabel.text( "Complete!" );
+        dialog.dialog( "option", "buttons", [{
+          text: "Close",
+          click: closeDownload
+        }]);
+        $(".ui-dialog button").last().trigger( "focus" );
+      }
+    });
+ 
+    function progress() {
+      var val = progressbar.progressbar( "value" ) || 0;
+ 
+      progressbar.progressbar( "value", val + Math.floor( Math.random() * 3 ) );
+ 
+      if ( val <= 99 ) {
+        progressTimer = setTimeout( progress, 50 );
+      }
+    }
+ 
+    function closeDownload() {
+      clearTimeout( progressTimer );
+      dialog
+        .dialog( "option", "buttons", dialogButtons )
+        .dialog( "close" );
+      progressbar.progressbar( "value", false );
+      progressLabel
+        .text( "Starting download..." );
+      downloadButton.trigger( "focus" );
+    }
+// pogress end
+
+// tabs start
+$( "#tabs" ).tabs();
+// tabs end
+
+
+});
